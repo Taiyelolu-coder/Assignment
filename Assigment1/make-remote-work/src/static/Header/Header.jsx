22b086f689c63b1a7ccc60a3166866ca54bb7323
Taiyelolu-coder <alayandeebudola@gmail.com>
2024-10-19 11:43:43 +0100

    my second push

--- Assigment1/make-remote-work/src/static/Header/Header.jsx
+++ Assigment1/make-remote-work/src/static/Header/Header.jsx
@@ -5,29 +5,31 @@ import IconMenu from"../../assets/icon-menu.svg"
 function Header (){
     return(
         <div className="container">
-           <div className="logo-nav">
-           
-                <div className="logoNav">
+
+            <div className="logoNav">
                 <img src={Logo} alt="logo" />
-                </div>   
-            <div className="link-wrapper">   
+            </div>   
+
+            <div className="link-wrapper">    
                 <div className="nav">
                     <nav>Feature</nav>
-                    
-                        <nav>Company</nav>
-                        <nav>Careers</nav>
-                        <nav>About</nav>
-                    </div>
+                    <nav>Company</nav>
+                    <nav>Careers</nav>
+                    <nav>About</nav>
+                            
                 </div>
+                     
+                    
                 <div className="loginBtn">
                     <nav>Login</nav>
                     <button className="btn1">Register</button>
                 </div>
-            
-            <div class="iconMenu">
-                <img src={IconMenu} alt="" />
+                    
+               
+            </div> 
+            <div id="iconMenu">
+                <img id="iconMenu"src={IconMenu} alt="" />
             </div>
-           </div> 
         </div>
     )
 }
--- Assignment2/coolors.co/src/components/Hero/Hero.jsx
+++ Assignment2/coolors.co/src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import "./Hero.css"
 import HeroImage from "../../assets/hero.png"
-import Producthunt from "../../assets/top-post-badge.svg"
+import ProductHunt from "../../assets/top-post-badge.svg"
 
 
 
@@ -14,11 +14,11 @@ function Hero() {
           <p>Create the perfect palette or get inspired by<br></br> thousands of beautiful color schemes</p>
           <button id="gen-btn">Start the generator</button>
           <button>Explore trending palettes</button>
-          <img src={Producthunt} alt="" /> 
+          <img src={ProductHunt} alt="" /> 
         </div>
         <div>
          <img id="hero-image" src={HeroImage} alt="" />
-         </div>
+        </div>
       </div>
     )
   }
