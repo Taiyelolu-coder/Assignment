9c70e72949325baf50a5a9006d54576df8318305
Taiyelolu-coder <alayandeebudola@gmail.com>
2024-10-19 10:32:46 +0100

    my second push

--- Assigment1/make-remote-work/src/static/Header/Header.jsx
+++ Assigment1/make-remote-work/src/static/Header/Header.jsx
@@ -1,26 +1,33 @@
 import"./Header.css"
 import Logo from"../../assets/logo.svg"
-
+import IconMenu from"../../assets/icon-menu.svg"
 
 function Header (){
     return(
         <div className="container">
            <div className="logo-nav">
-           <div className="logoNav">
-            <img src={Logo} alt="logo" />
-            </div>     
-           <div className="nav">
-               <nav>Feature</nav>
-               
-                <nav>Company</nav>
-                <nav>Careers</nav>
-                <nav>About</nav>
+           
+                <div className="logoNav">
+                <img src={Logo} alt="logo" />
+                </div>   
+            <div className="link-wrapper">   
+                <div className="nav">
+                    <nav>Feature</nav>
+                    
+                        <nav>Company</nav>
+                        <nav>Careers</nav>
+                        <nav>About</nav>
+                    </div>
+                </div>
+                <div className="loginBtn">
+                    <nav>Login</nav>
+                    <button className="btn1">Register</button>
+                </div>
+            
+            <div class="iconMenu">
+                <img src={IconMenu} alt="" />
             </div>
-           </div>
-           <div className="loginBtn">
-                <nav>Login</nav>
-                <button className="btn1">Register</button>
-           </div>
+           </div> 
         </div>
     )
 }
